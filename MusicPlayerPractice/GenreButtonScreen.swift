9c3232625fb0545fed2b5d444af1c01dc4ad335c
EhsanTgv <EhsanTgv@gmail.com>
2022-10-13 10:11:29 +0300

    Implement StoryBoard

---
+++ MusicPlayerPractice/GenreButtonScreen.swift
@@ -0,0 +1,29 @@
+//
+//  GenreButtonScreen.swift
+//  MusicPlayerPractice
+//
+//  Created by Ehsan Taghavi on 13.10.2022.
+//
+
+import UIKit
+
+class GenreButtonScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
